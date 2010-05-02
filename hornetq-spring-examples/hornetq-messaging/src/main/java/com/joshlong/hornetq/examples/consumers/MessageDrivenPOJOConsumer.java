8b804bff372458564080eb0f04ba8a6a9471eaed
Josh Long <josh@joshlong.com>
2010-05-02 10:19:42 +0000

    Adding a bunch of ways to consume messages using Spring

--- hornetq-spring-examples/hornetq-messaging/src/main/java/com/joshlong/hornetq/examples/consumers/MessageDrivenPOJOConsumer.java
+++ hornetq-spring-examples/hornetq-messaging/src/main/java/com/joshlong/hornetq/examples/consumers/MessageDrivenPOJOConsumer.java
@@ -3,6 +3,8 @@ package com.joshlong.hornetq.examples.consumers;
 import javax.jms.TextMessage;
 
 /**
+ * A simple MDP (Message-Driven POJOs, as opposed to Message Driven Beans from JEE). The MDP is configured using Spring and the jms: namespace support.
+ *
  * @author <a href="mailto:josh@joshlong.com">Josh Long</a>
  */
 public class MessageDrivenPOJOConsumer {
--- hornetq-spring-examples/hornetq-messaging/src/main/java/com/joshlong/hornetq/examples/consumers/SpringIntegrationServiceActivatorConsumer.java
+++ hornetq-spring-examples/hornetq-messaging/src/main/java/com/joshlong/hornetq/examples/consumers/SpringIntegrationServiceActivatorConsumer.java
@@ -5,6 +5,9 @@ import org.springframework.integration.annotation.ServiceActivator;
 import javax.jms.Message;
 
 /**
+ * This consumer leverages the Spring Integration service bus to "adapt" our code to the external middleware which - in this case - is HornetQ. By the time we receive it, the JMS message ({@link javax.jms.Message}) is the payload to an envelope {@link org.springframework.integration.core.Message}
+ * type that's native to the Spring Integration framework.
+ *
  * @author <a href="mailto:josh@joshlong.com">Josh Long</a>
  */
 public class SpringIntegrationServiceActivatorConsumer {
