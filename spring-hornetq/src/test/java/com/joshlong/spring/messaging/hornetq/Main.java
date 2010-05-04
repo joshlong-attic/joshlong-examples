c87d07935814b0e887112924f6ee590861896530
Josh Long <josh@joshlong.com>
2010-05-04 08:15:16 +0000

    Removing extraneous code

--- spring-hornetq/src/test/java/com/joshlong/spring/messaging/hornetq/Main.java
+++ spring-hornetq/src/test/java/com/joshlong/spring/messaging/hornetq/Main.java
@@ -1,18 +1,13 @@
 package com.joshlong.spring.messaging.hornetq;
 
 import org.apache.commons.lang.builder.ToStringBuilder;
-
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.core.MessageCreator;
-
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
-
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.Session;
@@ -38,7 +33,7 @@ public class Main {
                     return session.createTextMessage(String.format("Hello, world! @ %s", System.currentTimeMillis() + ""));
                 }
             });
-        
+
         // receive
         Message msg = jmsTemplate.receive(destination);
         System.out.println(ToStringBuilder.reflectionToString(msg));
