5455d7327ead117fd50902a2f7d1c0275cd3dc36
Josh Long <jolong@jlong-mbpro.local>
2010-08-09 15:36:13 -0700

    adding updated code and spring context files

--- spring-activiti-example/src/main/java/com/joslong/examples/activiti/ActivitiClient.java
+++
@@ -1,7 +0,0 @@
-package com.joshlong.examples.activiti;
-
-public class ActivitiClient {
-    public static void main(String[] args) throws Throwable {
-        System.out.println("hello, world.");
-    }
-}
