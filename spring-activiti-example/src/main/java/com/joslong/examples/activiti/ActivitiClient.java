6dda3f37d8eee3ad3f0588b93b40dbfa14f54d92
Josh Long <jolong@jlong-mbpro.local>
2010-08-09 14:25:16 -0700

    adding oinitial simple code

---
+++ spring-activiti-example/src/main/java/com/joslong/examples/activiti/ActivitiClient.java
@@ -0,0 +1,7 @@
+package com.joshlong.examples.activiti;
+
+public class ActivitiClient {
+    public static void main(String[] args) throws Throwable {
+        System.out.println("hello, world.");
+    }
+}
