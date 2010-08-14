3fee8ebf3f6e6d8b5861e730e03b46076a5c373b
Josh Long <josh@joshlong.com>
2010-08-14 03:50:29 -0700

    adding new files related to testing Spring Integration gateway.

--- spring-activiti-example/src/main/java/com/joshlong/examples/SpringIntegrationActivityBehaviorMessagingGateway.java
+++ spring-activiti-example/src/main/java/com/joshlong/examples/SpringIntegrationActivityBehaviorMessagingGateway.java
@@ -1,37 +1,36 @@
 package com.joshlong.examples;
 
 import org.activiti.ProcessEngine;
-
 import org.activiti.impl.ProcessEngineImpl;
 import org.activiti.impl.cmd.SendEventCmd;
 import org.activiti.impl.interceptor.CommandExecutor;
-
 import org.activiti.pvm.ActivityBehavior;
 import org.activiti.pvm.ActivityExecution;
-
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Required;
-
 import org.springframework.integration.Message;
 import org.springframework.integration.core.MessageBuilder;
 import org.springframework.integration.gateway.AbstractMessagingGateway;
+import org.springframework.integration.gateway.SimpleMessagingGateway;
+import org.springframework.stereotype.Component;
 
 
 /**
  * This class is plugged into an Activit workflow. <serviceTask /> let's us plugin a custom {@link org.activiti.pvm.ActivityBehavior}.
- * <p/>
- * We need to build an {@link org.activiti.pvm.ActivityBehavior} that can send and receive the message, propagating the {@code executionId}
- * and potentially process variables/ header variables.
+ *
+ * We need to build an {@link org.activiti.pvm.ActivityBehavior} that can send and receive the message, propagating the {@code executionId} and potentially process variables/ header variables.
  *
  * @author Josh Long (based on an idea sketched out w/ Dave Syer)
+ *
  * @see org.activiti.impl.bpmn.ReceiveTaskActivity this the ActivityBehavior impl that ships w/ Activiti that has the machinery to wake up when signal'd.
  */
-public class SpringIntegrationActivityBehaviorMessagingGateway extends AbstractMessagingGateway implements ActivityBehavior, InitializingBean {
+@Component
+public class SpringIntegrationActivityBehaviorMessagingGateway extends SimpleMessagingGateway implements ActivityBehavior, InitializingBean {
     public static final String WELL_KNOWN_EXECUTION_ID_HEADER_KEY = "executionId";
     private ProcessEngine processEngine;
     private CommandExecutor commandExecutor;
 
+    @Required
     public void setProcessEngine(ProcessEngine processEngine) {
         this.processEngine = processEngine;
     }
@@ -52,6 +51,7 @@ public class SpringIntegrationActivityBehaviorMessagingGateway extends AbstractM
         if (processEngine instanceof ProcessEngineImpl) {
             this.commandExecutor = ((ProcessEngineImpl) processEngine).getProcessEngineConfiguration().getCommandExecutor();
         }
+
         super.doStart();
     }
 }
--- spring-activiti-example/src/main/java/com/joshlong/examples/activiti/spring/CustomServiceTaskClient.java
+++ spring-activiti-example/src/main/java/com/joshlong/examples/activiti/spring/CustomServiceTaskClient.java
@@ -1,30 +1,35 @@
 package com.joshlong.examples.activiti.spring;
 
 import org.activiti.ProcessEngine;
+import org.activiti.ProcessService;
+
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
- 
+
+
 /**
- *
  * This class handles proving that we can forward an exection to Spring integration
- *
  *
  * @author Josh Long
  */
-@Component
+ 
 public class CustomServiceTaskClient {
+    @Autowired
+    private ProcessEngine processEngine;
+
     @PostConstruct
     public void begin() throws Throwable {
-
+        ProcessService processService = processEngine.getProcessService();
+        processService.createDeployment().addClasspathResource("processes/sigateway1.bpmn20.xml").deploy();
     }
 
-    @Autowired private ProcessEngine processEngine ;
-       public static void main(String[] args) throws Throwable {
-        ClassPathXmlApplicationContext classPathXmlApplicationContext
-                = new ClassPathXmlApplicationContext("d2.xml");
-        classPathXmlApplicationContext.start();
+    public static void main(String[] args) throws Throwable {
+        ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext("d3.xml");
+      ///  classPathXmlApplicationContext.start();
     }
 }
