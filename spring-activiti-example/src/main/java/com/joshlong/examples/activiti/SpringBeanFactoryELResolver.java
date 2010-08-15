42613b166aff82176f8a239a990f61cd0d745ccb
Josh Long <josh@joshlong.com>
2010-08-14 21:11:57 -0700

    adding / reworking the ELResolver implementations.

--- spring-activiti-example/src/main/java/com/joshlong/examples/activiti/SpringBeanFactoryELResolver.java
+++ spring-activiti-example/src/main/java/com/joshlong/examples/activiti/SpringBeanFactoryELResolver.java
@@ -1,7 +1,18 @@
-package com.joshlong.examples;
+/* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.joshlong.examples.activiti;
 
 import org.activiti.impl.el.StaticElResolver;
-import org.activiti.pvm.ActivityBehavior;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ApplicationContext;
@@ -11,14 +22,9 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 
-/**
- * This simply provides access to every {@link org.activiti.pvm.ActivityBehavior} in the bean factory
- *
- * @author Josh Long
- */
 public class SpringBeanFactoryELResolver extends StaticElResolver implements ApplicationContextAware, InitializingBean {
 
-    private ApplicationContext applicationContext;
+    protected ApplicationContext applicationContext;
 
     private Map<String, Object> map = new ConcurrentHashMap<String, Object>();
 
@@ -28,7 +34,7 @@ public class SpringBeanFactoryELResolver extends StaticElResolver implements App
     }
 
     public void afterPropertiesSet() throws Exception {
-        for (String beanName : this.applicationContext.getBeanNamesForType(ActivityBehavior.class))
+        for (String beanName : this.applicationContext.getBeanDefinitionNames() )
             this.map.put(beanName, this.applicationContext.getBean(beanName));
 
         this.setMap(this.map);
--- spring-activiti-example/src/main/java/com/joshlong/examples/activiti/gateway/activiti/ActivityBehaviorMessagingGateway.java
+++ spring-activiti-example/src/main/java/com/joshlong/examples/activiti/gateway/activiti/ActivityBehaviorMessagingGateway.java
@@ -1,33 +1,28 @@
+/* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.joshlong.examples.activiti.gateway.activiti;
 
 import org.activiti.ProcessEngine;
-
-import org.activiti.impl.ProcessEngineImpl;
-import org.activiti.impl.cmd.SendEventCmd;
 import org.activiti.impl.db.execution.DbExecutionImpl;
-import org.activiti.impl.interceptor.CommandExecutor;
-
 import org.activiti.pvm.ActivityBehavior;
 import org.activiti.pvm.ActivityExecution;
-
-import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.builder.ToStringBuilder;
-
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.InitializingBean;
-
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
-
 import org.springframework.integration.Message;
 import org.springframework.integration.MessageHeaders;
 import org.springframework.integration.core.MessageBuilder;
 import org.springframework.integration.gateway.SimpleMessagingGateway;
 
-import org.springframework.util.Assert;
-
 import java.util.Map;
-import java.util.logging.Logger;
 
 
 /**
@@ -45,35 +40,34 @@ import java.util.logging.Logger;
  * </LI>
  * </OL>
  *
- * @author Josh Long (based on an idea sketched out w/ Dave Syer and Tom Baeyans)
+ * @author Josh Long (based on an idea sketched out w/ Dave Syer and Tom Baeyens)
  * @see org.activiti.impl.bpmn.ReceiveTaskActivity this the ActivityBehavior impl that ships w/ Activiti that has the machinery to wake up when signal'd.
  */
-public class ActivityBehaviorMessagingGateway extends SimpleMessagingGateway implements ActivityBehavior  {
+public class ActivityBehaviorMessagingGateway extends SimpleMessagingGateway implements ActivityBehavior {
+
     public static final String WELL_KNOWN_EXECUTION_ID_HEADER_KEY = "activiti_spring_integration_executionId";
-    private ProcessEngine processEngine;
-    //private CommandExecutor commandExecutor;
+
+    /**
+     * Injected from Spring or some other mechanism. Recommended approach is through a {@link org.activiti.impl.cfg.spring.ProcessEngineFactoryBean}
+     */
+    private volatile ProcessEngine processEngine;
+
+    /**
+     * Should we update the process variables based on the reply {@link org.springframework.integration.Message}'s {@link org.springframework.integration.MessageHeaders}?
+     */
+    private volatile boolean updateProcessVariablesFromResponseMessageHeaders;
+
+    /**
+     * Should we pass the workflow process variables as message headers when we send a message into the Spring Integration framework?
+     */
+    private volatile boolean forwardProcessVariablesAsMessageHeaders;
 
     public void setProcessEngine(ProcessEngine processEngine) {
         this.processEngine = processEngine;
     }
 
-    private boolean updateProcessVariablesFromResponseMessageHeaders;
-    private boolean forwardProcessVariablesAsMessageHeaders;
-
     public void setForwardProcessVariablesAsMessageHeaders(boolean forwardProcessVariablesAsMessageHeaders) {
         this.forwardProcessVariablesAsMessageHeaders = forwardProcessVariablesAsMessageHeaders;
-    }
-
-
-    /*  private void receiveFromSpringIntegration(Message<?> msgFromSI) {
-        String executionId = (String) msgFromSI.getHeaders().get(WELL_KNOWN_EXECUTION_ID_HEADER_KEY);
-        this.commandExecutor.execute(new SendEventCmd(executionId, null));
-    }*/
-    @Override
-    protected void onInit() throws Exception {
-   /*     if (processEngine instanceof ProcessEngineImpl) {
-            this.commandExecutor = ((ProcessEngineImpl) processEngine).getProcessEngineConfiguration().getCommandExecutor();
-        }*/
     }
 
     public void setUpdateProcessVariablesFromResponseMessageHeaders(boolean updateProcessVariablesFromResponseMessageHeaders) {
@@ -82,7 +76,7 @@ public class ActivityBehaviorMessagingGateway extends SimpleMessagingGateway imp
 
     public void execute(ActivityExecution execution) throws Exception {
         if (this.logger.isDebugEnabled()) {
-            logger.debug("Starting execute(ActivityExecution) " + ToStringBuilder.reflectionToString(execution));
+            logger.debug("Entering execute(ActivityExecution) " + ToStringBuilder.reflectionToString(execution));
         }
 
         String executionId = null;
@@ -90,8 +84,6 @@ public class ActivityBehaviorMessagingGateway extends SimpleMessagingGateway imp
         if (execution instanceof DbExecutionImpl) {
             executionId = ((DbExecutionImpl) execution).getId();
         }
-
-        Assert.state(!StringUtils.isEmpty(executionId), "the 'executionId' can't be null!"); // though, technically, it can since we're not necessarily using it to revive any records asynchronously
 
         MessageBuilder<?> messageBuilder = MessageBuilder.withPayload(execution).setHeader(WELL_KNOWN_EXECUTION_ID_HEADER_KEY, executionId).setCorrelationId(executionId);
 
@@ -115,7 +107,7 @@ public class ActivityBehaviorMessagingGateway extends SimpleMessagingGateway imp
         }
 
         if (this.logger.isDebugEnabled()) {
-            logger.debug("Execiting execute(ActivityExecution) " + ToStringBuilder.reflectionToString(execution));
+            logger.debug("Exiting execute(ActivityExecution) " + ToStringBuilder.reflectionToString(execution));
         }
     }
 }
