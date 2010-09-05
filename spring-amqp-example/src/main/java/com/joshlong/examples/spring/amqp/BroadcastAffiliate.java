4e713dd3383df78d17d0a908a4fb1cf62581f53a
Josh Long <josh@joshlong.com>
2010-09-05 05:45:14 -0700

    adding the simple seeds of my example.

--- spring-amqp-example/src/main/java/com/joshlong/examples/spring/amqp/BroadcastAffiliate.java
+++ spring-amqp-example/src/main/java/com/joshlong/examples/spring/amqp/BroadcastAffiliate.java
@@ -1,14 +1,31 @@
 package com.joshlong.examples.spring.amqp;
 
-public class BroadcastAffiliate {
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.stereotype.Component;
 
-	/**
-	 * Gets called when a {@link NewsChannel} publishes news that needs to be received and re-broadcast appropriately.
-	 *
-	 * @param newsStory the news story
-	 * @throws Exception   escape-hatch {@link com.joshlong.examples.spring.amqp.NewsStory}
-	 */
-	public void breakForNews( NewsStory newsStory ) throws Exception {
-		
-	}
+import javax.annotation.PostConstruct;
+
+
+@Component
+public class BroadcastAffiliate {
+    private AmqpTemplate amqpTemplate;
+
+    public void setAmqpTemplate(AmqpTemplate amqpTemplate) {
+        this.amqpTemplate = amqpTemplate;
+    }
+
+    @PostConstruct
+    public void establishListener() throws Exception {
+        System.out.println("establishListener");
+    }
+
+    /**
+     * Gets called when a {@link NewsChannel} publishes news that needs to be received and re-broadcast appropriately.
+     *
+     * @param newsStory the news story
+     * @throws Exception escape-hatch {@link com.joshlong.examples.spring.amqp.NewsStory}
+     */
+    public void breakForNews(NewsStory newsStory) throws Exception {
+        System.out.println("this just in...  " + newsStory);
+    }
 }
